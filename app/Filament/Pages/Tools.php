e6d7687e9d60cc098b4121d3058fe37b9cfacd19
Muhamad Absor Dwiyana <127928756+absorkun@users.noreply.github.com>
2026-03-11 08:09:32 +0700

    Update export logic

--- app/Filament/Pages/Tools.php
+++ app/Filament/Pages/Tools.php
@@ -6,10 +6,13 @@ use App\Jobs\ExportNameserverIps;
 use App\Models\Domain;
 use Filament\Pages\Page;
 use Illuminate\Database\Eloquent\Builder;
+use BackedEnum;
 
 class Tools extends Page
 {
     protected string $view = 'filament.pages.tools';
+
+    protected static BackedEnum | string | null $navivationIcon = 'heroicon-o-wrench-screwdriver';
     
     public function generateCsv()
     {
